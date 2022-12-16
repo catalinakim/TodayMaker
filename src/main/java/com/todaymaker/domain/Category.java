660f77faca399aa6a2560f7ffa70ed706b6e6484
sesangmak@gmail.com <sesangmak@gmail.com>
2022-12-16 20:28:53 +0900

    카테고리에 부모카테고리 추가

--- src/main/java/com/todaymaker/domain/Category.java
+++ src/main/java/com/todaymaker/domain/Category.java
@@ -1,24 +1,37 @@
 package com.todaymaker.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
+@Getter @Setter
+@NoArgsConstructor
 public class Category {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
+
+    @ManyToOne(fetch = LAZY)
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    @OneToMany(mappedBy = "category")
+    private List<Todo> todos = new ArrayList<>();
 
     public Category(String name) {
         this.name = name;
     }
 
-    @OneToMany(mappedBy = "category")
-    private List<Todo> todos = new ArrayList<>();
 
-    public Category() {
-    }
 }
