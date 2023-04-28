b9d05d4ec4db6fd40f37fc322f73b0ab490de6c4
sesangmak@gmail.com <sesangmak@gmail.com>
2023-04-29 07:30:14 +0900

    회원가입 테스트코드 작성

---
+++ src/test/java/com/todaymaker/service/UserServiceTest.java
@@ -0,0 +1,32 @@
+package com.todaymaker.service;
+
+import com.todaymaker.domain.User;
+import com.todaymaker.repository.UserJpaRepository;
+import com.todaymaker.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class UserServiceTest {
+
+    @Autowired EntityManager em;
+    @Autowired UserJpaRepository userJpaRepository;
+    @Autowired UserRepository userRepository;
+    @Autowired UserService userService;
+
+    @Test
+    void 회원가입() {
+        User savedUser = userService.save(new User("aaa", "1234", "aaa@a.ai"));
+        Assertions.assertEquals(savedUser, userJpaRepository.findOne(savedUser.getId()));
+    }
+}
