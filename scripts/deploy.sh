d451012d7c6c2248b77daa4672f9eba483602553
sesangmak@gmail.com <sesangmak@gmail.com>
2023-05-02 19:07:43 +0900

    배포 경로 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/action/build/libs/*.jar)
+BUILD_JAR=$(ls /home/ubuntu/today/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/today/deploy.log
 
-echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
-DEPLOY_PATH=/home/ubuntu/action/
+echo "> build 파일 복사" >> /home/ubuntu/today/deploy.log
+DEPLOY_PATH=/home/ubuntu/today/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/today/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료X" >> /home/ubuntu/today/deploy.log
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -21,4 +21,4 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/today/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ubuntu/today/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/today/deploy.log 2>/home/ubuntu/today/deploy_err.log &
